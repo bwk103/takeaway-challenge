25164ed01fec31eb09d12bc9d0050b839a73b817
Ben Kielty <bwk103@gmail.com>
2017-12-24 17:38:10 +0000

    Create Dish class
    Created dish class.  Instances are initialized with a name and price
    only.  All unit tests pass.

---
+++ spec/dish_spec.rb
@@ -0,0 +1,20 @@
+require 'dish'
+
+describe Dish do
+
+  let(:dish) { described_class.new(name: 'Cheese Pizza', price: 5.00) }
+
+  context "defaults" do
+
+    it "has a name" do
+      name = 'Cheese Pizza'
+      expect(dish.name).to eq name
+    end
+
+    it "has a price" do
+      price = 5.00
+      expect(dish.price).to eq price
+    end
+  end
+
+end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,5 +4,6 @@ describe Takeaway do
 
   describe "#view_menu" do
     it { is_expected.to respond_to :view_menu }
+
   end
 end
